02cc9ba2264074ab1aa41ea29ce32f6b0d4c539d
Soonsang <npmachine@dost11.com>
2025-03-05 23:57:22 +0900

    build: remove static option

--- next.config.js
+++ next.config.js
@@ -4,30 +4,6 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts') // app 폴더를 
 // -> src 폴더를 사용하는 경우 인자를 생략합니다.
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /**
-   * Enable static exports.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: 'export',
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  // basePath: '',
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
-}
+const nextConfig = {}
 
 module.exports = withNextIntl(nextConfig)
