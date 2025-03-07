d4057196c7b551033d6b362f038800cd0e738519
npmachine <engineered.design@gmail.com>
2025-03-07 15:29:19 +0900

    chore: add footer translation

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,21 +1,33 @@
-import Link from "next/link"
+import { useTranslations } from "next-intl"
 
 export default function Footer() {
+  const t = useTranslations("footer")
+
   return (
     <footer className="bg-background border-t">
       <div className="container mx-auto py-8 px-4">
         <div className="text-center text-sm text-white">
-          <p className="mb-4">Dost11 Inc.</p>
-          <p className="mb-4">
-            <span className="mx-2">CEO: Hyogeol Yang</span>
-            <span className="mx-2">|</span>
-            <span className="mx-2">Business Registration Number: 478-87-02968</span>
+          <p className="sm:mb-4">
+            {t("company name")}
           </p>
-          <p className="mb-4">
-            <span className="mx-2">Email: hello@dost11.com</span>
+          <p className="sm:mb-4 flex flex-col sm:flex-row justify-center items-center">
+            <span className="mx-2">
+              {t("ceo")}
+            </span>
+            <span className="hidden sm:block mx-2">|</span>
+            <span className="mx-2">
+              {t("brn")}
+            </span>
           </p>
-          <p className="mb-6">
-            <span className="mx-2">Address: 11F, 255 Seongam-ro, Mapo-gu, Seoul, Korea</span>
+          <p className="sm:mb-4">
+            <span className="mx-2">
+              {t("email")}
+            </span>
+          </p>
+          <p className="mb-2 sm:mb-6">
+            <span className="mx-2">
+              {t("address")}
+            </span>
           </p>
         </div>
       </div>
